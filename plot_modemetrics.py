4b821235846571e55335b79b078f834973604848
Maarten Hugenholtz <maarten.hhz@gmail.com>
2024-05-11 09:15:08 +0200

    make subplot and table for stats

--- plot_modemetrics.py
+++ plot_modemetrics.py
@@ -16,7 +16,9 @@ models_result_paths = ['interaction_mode_metrics_val.csv',
 
 # combine data into one df:
 dfs = []
+dfs_stats = []
 consistencies = []
+
 for model, path in zip(models, models_result_paths):
     df_temp = pd.read_csv(path).dropna()
     df_temp['model'] = model
@@ -27,6 +29,8 @@ for model, path in zip(models, models_result_paths):
     # print stats
     df_notcorrect = df_temp[df_temp['metric t2cor'] < Hpred]
     df_0scor = df_temp[df_temp['metric t2cor'] ==0]
+    df_notcovered = df_temp[df_temp['metric t2cov'] < Hpred]
+    df_0scov = df_temp[df_temp['metric t2cov'] ==0]
 
     print(f'MODEL: {model}')
     print(f'{len(df_temp)} interactions in dataset')
@@ -38,19 +42,31 @@ for model, path in zip(models, models_result_paths):
     print(f"Average mode-collapse = {round(df_temp['r_mode_collapse'].mean(),1)}%")
     print()
 
+    df_stats_model = pd.DataFrame({
+        'model': [model],
+        'ML prediction consistency': [r_pred_consistency],
+        'incorrect a2a predictions':[ round(100 * len(df_notcorrect) / len(df_temp),1)],
+        'mean time correct a2a mode': [df_notcorrect['t2cor'].mean()],
+        'percentage predictions @ t2cor = 0s': round(100*len(df_0scor)/len(df_temp), 1),
+        'uncovered a2a predictions': [ round(100 * len(df_notcovered) / len(df_temp),1)],
+        'mean time covered a2a mode': [df_notcovered['t2cov'].mean()],
+        'percentage predictions @ t2cov = 0s': [round(100*len(df_0scov)/len(df_temp), 1)],
+    })
+    dfs_stats.append(df_stats_model)
 
 df_combined = pd.concat(dfs)
+df_stats = pd.concat(dfs_stats)
 
 
 
-fig = px.histogram(df_combined, x='r_mode_collapse', color = 'model', barmode= 'group',nbins=11, title='mode collapse ratio')
-fig.show()
+# fig = px.histogram(df_combined, x='r_mode_collapse', color = 'model', barmode= 'group',nbins=11, title='mode collapse ratio')
+# fig.show()
 
-fig = px.histogram(df_combined, x='metric t2cor',color = 'model', barmode= 'group',nbins=10, title='time to correct prediction [s]')
-fig.show()
+# fig = px.histogram(df_combined, x='metric t2cor',color = 'model', barmode= 'group',nbins=10, title='time to correct prediction [s]')
+# fig.show()
 
-fig = px.histogram(df_combined, x='metric t2cov',color = 'model', barmode= 'group',nbins=10, title='time to covered prediction [s]')
-fig.show()
+# fig = px.histogram(df_combined, x='metric t2cov',color = 'model', barmode= 'group',nbins=10, title='time to covered prediction [s]')
+# fig.show()
 
 # # Create subplots
 # fig = make_subplots(rows=1, cols=4, subplot_titles=('Time to Correct Prediction [s]', 'Time to Covered Prediction [s]', 'Mode Collapse Ratio', 'Prediction Consistency'))
@@ -64,3 +80,31 @@ fig.show()
 
 
 # fig.show()
+
+fig = make_subplots(rows=1, cols=3, subplot_titles=('Time to Correct Prediction [s]', 'Time to Covered Prediction [s]','Mode Collapse Ratio [%]', 'ML prediction consistency [%]'))
+colors = px.colors.qualitative.Plotly
+
+# Iterate through unique model values and create histogram for each
+for i, model in enumerate(df_combined['model'].unique(), start=1):
+    # Filter data for current model
+    data_model = df_combined[df_combined['model'] == model]
+    
+    # Add histograms to subplots
+    fig.add_trace(
+        go.Histogram(x=data_model['metric t2cor'], nbinsx=10, 
+                     name=model, marker_color=colors[i-1], legendgroup=model, showlegend=False),
+        row=1, col=1
+    )
+    fig.add_trace(
+        go.Histogram(x=data_model['metric t2cov'], nbinsx=10,
+                     name=model, marker_color=colors[i-1], legendgroup=model, showlegend=False),
+        row=1, col=2
+    )
+    fig.add_trace(
+        go.Histogram(x=data_model['r_mode_collapse'], nbinsx=11,
+                     name=model, marker_color=colors[i-1], legendgroup=model),
+        row=1, col=3
+    )
+
+
+fig.show()
